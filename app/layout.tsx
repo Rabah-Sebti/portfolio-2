47f82e7a2fff0671cad0484c4ea957ffb53136ca
rabah <sebtirabah19@gmail.com>
2025-10-27 10:54:34 +0100

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { HOST_URL } from "@/constants/config";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,10 +13,48 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Rabah Sebti",
-  description: "Rabah Sebti Web Developer Portfolio",
-};
+export async function generateMetadata() {
+  // read route params
+
+  return {
+    title: {
+      template: "%s | Rabah Sebti",
+      default: "Rabah Sebti",
+    },
+    description: "Rabah Sebti Web Developer Portfolio",
+    icons: {
+      icon: "/images/profile.jpg",
+    },
+    openGraph: {
+      title: "Rabah Sebti Web Developer Portfolio",
+      description: "Rabah Sebti Web Developer Portfolio",
+      url: HOST_URL,
+      siteName: "Rabah Sebti Web Developer Portfolio",
+      images: [
+        {
+          url: `${HOST_URL}/images/profile.jpg`, // Must be an absolute URL
+        },
+      ],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Rabah Sebti Web Developer Portfolio",
+      description: "Rabah Sebti Web Developer Portfolio",
+
+      images: [`${HOST_URL}/images/profile.jpg`], // Must be an absolute URL
+    },
+
+    alternates: {
+      canonical: `${HOST_URL}`,
+    },
+  };
+}
+
+// export const metadata: Metadata = {
+//   title: "Rabah Sebti",
+//   description: "Rabah Sebti Web Developer Portfolio",
+// };
 
 export default function RootLayout({
   children,
---
+++ constants/config.ts
@@ -0,0 +1,1 @@
+export const HOST_URL = "https://rabah-sebti-portfolio.netlify.app/";
