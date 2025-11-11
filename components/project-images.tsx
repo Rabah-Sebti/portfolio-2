cafbfa9b884e3d5bc4e1af6f5ba9e8e943b94ac2
rabah <sebtirabah19@gmail.com>
2025-11-11 10:13:45 +0100

    resolve image select in project page

--- components/project-images.tsx
+++ components/project-images.tsx
@@ -41,9 +41,7 @@ function ProjectImages({ thumbnail, images, title }: ProjectImagesProps) {
           alt="thumbnail"
           height="1000"
           width="1000"
-          className="rounded-md object-contain"
-          // placeholder="blur"
-          // blurDataURL=""
+          className="rounded-md object-cover mdd:object-fill h-[200px] md:h-[350px] w-full"
         />
         {/* <div className="absolute bottom-0 bg-white h-40 w-full [mask-image:linear-gradient(to_bottom,transparent,white)]" /> */}
       </motion.div>
